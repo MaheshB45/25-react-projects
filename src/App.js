6ee6d9a14efa0b103fc8d44e7b0dbd101866ad06
Mahesh Bahir <maheshbahir45@gmail.com>
2025-01-02 17:07:19 +0530

    create image-slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -15,6 +15,13 @@ function App() {
 
       {/* StarRating component */}
       <StarRating />
+
+      {/* ImageSlider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
