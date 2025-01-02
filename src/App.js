cbad09ae0f664e0bff38ebd6a93dbf3e644e6c90
Mahesh Bahir <maheshbahir45@gmail.com>
2025-01-02 14:36:21 +0530

    adding 3 task

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian component */}
+      <Accordian />
+
+      {/* RandomColor component */}
+      <RandomColor />
+
+      {/* StarRating component */}
+      <StarRating />
+    </div>
+  );
+}
+
+export default App;
