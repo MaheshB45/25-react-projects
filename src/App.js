c141bec1f7c42ed55cbfa514dd47c389d1debeec
Mahesh Bahir <maheshbahir45@gmail.com>
2025-01-03 18:44:11 +0530

    add load-more-data task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Accordian from "./components/accordian";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
+      <h1>Machine Code Interview Tasks</h1>
+
       {/* Accordian component */}
       <Accordian />
 
@@ -22,6 +25,9 @@ function App() {
         page={"1"}
         limit={"10"}
       />
+
+      {/* LoadMoreData component */}
+      <LoadMoreData />
     </div>
   );
 }
