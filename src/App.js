d3695433ebe825a9a16a9c8415c281f795db405a
Mahesh Bahir <maheshbahir45@gmail.com>
2025-01-04 16:57:22 +0530

    create TreeView component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -28,6 +30,9 @@ function App() {
 
       {/* LoadMoreData component */}
       <LoadMoreData />
+
+      {/* Tree view component/menu UI component / recursive navigation menu */}
+      <TreeView menus={menus}/>
     </div>
   );
 }
